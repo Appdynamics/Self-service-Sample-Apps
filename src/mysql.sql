ee6b45744e9269e6d33fed1f9c9256f479770dc9
Shawn Van Ittersum <svicalifornia@gmail.com>
2015-04-15 04:17:26 -0700

    Changes initial MySQL data

--- src/mysql.sql
+++ src/mysql.sql
@@ -2,13 +2,13 @@ CREATE DATABASE IF NOT EXISTS AppDemo;
 GRANT ALL PRIVILEGES ON AppDemo.* TO demouser@localhost IDENTIFIED BY 'demouser';
 FLUSH PRIVILEGES;
 USE AppDemo;
+DROP TABLE IF EXISTS products;
 CREATE TABLE IF NOT EXISTS products (
   id int NOT NULL AUTO_INCREMENT,
   name VARCHAR(255) NOT NULL UNIQUE,
   stock int NOT NULL DEFAULT 0,
   PRIMARY KEY (id)
 );
-INSERT INTO products (name, stock) SELECT 'Initial Post A', 100 FROM DUAL WHERE NOT EXISTS (SELECT name FROM products WHERE name = 'Initial Post A');
-INSERT INTO products (name, stock) SELECT 'Initial Post B', 50 FROM DUAL WHERE NOT EXISTS (SELECT name FROM products WHERE name = 'Initial Post B');
-INSERT INTO products (name, stock) SELECT 'Initial Post C', 1000 FROM DUAL WHERE NOT EXISTS (SELECT name FROM products WHERE name = 'Initial Post C');
-INSERT INTO products (name, stock) SELECT 'Initial Post D', 10 FROM DUAL WHERE NOT EXISTS (SELECT name FROM products WHERE name = 'Initial Post D');
+INSERT INTO products (name, stock) SELECT 'Product A', 100;
+INSERT INTO products (name, stock) SELECT 'Product B', 50;
+INSERT INTO products (name, stock) SELECT 'Product C', 500;
